7f564b2172c2829769092f60d18c2d8eeb4dffa1
2019barrachija <jose-agustin.barrachina@centralesupelec.fr>
2020-01-29 16:10:16 +0100

    Monte carlo simulation for gilles data

--- src/gilles_signals.py
+++ src/gilles_signals.py
@@ -2,13 +2,16 @@ import data_processing as dp
 import data_analysis as da
 import cvnn_v1_compat as cvnn
 import numpy as np
+import tensorflow as tf
 import sys
+import os
 from pdb import set_trace
 
 
 def load_gilles_mat_data(fname, default_path="/media/barrachina/data/gilles_data/"):
     mat = dp.load_matlab_matrices(fname, default_path)
     ic = mat['ic'].squeeze(axis=0)  # Labels corresponding to types
+    ic = ic - 1                     # Changed from matlab indexing to python (start with 0 and not with 1)
     nb_sig = mat['nb_sig'].squeeze(axis=0)  # number of examples for each label (label=position_ic)
     sx = mat['sx'][0]  # Unknown scalar
     types = [t[0] for t in mat['types'].squeeze(axis=0)]  # labels legends
@@ -37,26 +40,67 @@ def run_mlp(x_train, y_train, x_test, y_test, type=np.complex64):
 
     # Network creation
     mlp = cvnn.Cvnn("Gilles_net_complex", automatic_restore=False, logging_level="INFO")
-    mlp.create_mlp_graph("categorical_crossentropy",
+    mlp.create_mlp_graph(tf.keras.losses.categorical_crossentropy,
                          [(input_size, 'ignored'),
-                          (h1_size, 'cart_relu'),
-                          (h2_size, 'cart_relu'),
+                          (h1_size, 'cart_selu'),
+                          (h2_size, 'cart_selu'),
                           (output_size, 'cart_softmax_real')],
                          input_dtype=type)
 
     mlp.train(x_train, y_train, x_test, y_test, epochs=100, batch_size=100, display_freq=1000)
-    print(da.categorical_confusion_matrix(mlp.predict(x_test), y_test))
-    mlp.plot_loss_and_acc()
-    return mlp
+    # print(da.categorical_confusion_matrix(mlp.predict(x_test), y_test))
+    # mlp.plot_loss_and_acc()
+    return mlp.compute_loss(x_test, y_test), mlp.compute_accuracy(x_test, y_test)
 
 
-if __name__ == '__main__':
+def monte_carlo_comparison(x_train, y_train, x_test, y_test, iterations=10, path='./results/', filename='CVNNvsRVNN'):
+    write = True
+    if not os.path.exists(path):
+        os.makedirs(path)
+    if os.path.exists(path + filename + '.csv'):
+        write = False  # Not to write again the CVNN and all the headers.
+    file = open(path + filename + '.csv', 'a')
+    if write:
+        file.write("CVNN loss,CVNN acc,RVNN loss,RVNN acc\n")
+    for i in range(iterations):
+        print("Iteration: " + str(i) + "/" + str(iterations))
+        # runs network
+        cvloss, cvacc = run_mlp(x_train, y_train, x_test, y_test, np.complex64)     # Test complex one
+        rvloss, rvacc = run_mlp(x_train, y_train, x_test, y_test, np.float32)       # Test real one
+
+        # save result
+        file.write(str(cvloss) + "," + str(cvacc) + "," + str(rvloss) + "," + str(rvacc) + "\n")
+    file.close()
+
+
+def train_monte_carlo(data_name):
     # gets data
-    ic, nb_sig, sx, types, xp, xx = load_gilles_mat_data("data_cnn1dT.mat")
+    ic, nb_sig, sx, types, xp, xx = load_gilles_mat_data(data_name)
     cat_ic = dp.sparse_into_categorical(ic, num_classes=len(types))  # TODO: make sparse crossentropy test
     x_train, y_train, x_test, y_test = dp.separate_into_train_and_test(xx, cat_ic, pre_rand=True)
 
-    # runs network
-    run_mlp(x_train, y_train, x_test, y_test, np.float32)     # Test complex one
-    run_mlp(x_train, y_train, x_test, y_test, np.complex64)       # Test real one
-    set_trace()
+    # Train networks
+    monte_carlo_comparison(x_train, y_train, x_test, y_test, filename=os.path.splitext(data_name)[0])
+    print("Monte carlo finished")
+
+
+if __name__ == '__main__':
+    data_2chirps = "data_cnn1d.mat"
+    data_all_classes = "data_cnn1dC.mat"
+    data_2chirps_test = "data_cnn1dT.mat"
+
+    data_name = data_2chirps_test
+    # train_monte_carlo(data_2chirps_test)
+
+    # Show results
+    d_mean = da.get_loss_and_acc_means("./results/" + data_name.replace(".mat", ".csv"))
+    d_std = da.get_loss_and_acc_std("./results/" + data_name.replace(".mat", ".csv"))
+    da.plot_2_gaussian(d_mean['CVNN acc'], d_std['CVNN acc'], d_mean['RVNN acc'], d_std['RVNN acc'], 'CVNN', 'RVNN',
+                       x_label='accuracy', loc='upper right',
+                       title='RVNN vs CVNN accuracy for ' + os.path.splitext(data_name)[0] + ' dataset',
+                       filename="./results/accuracy_" + data_name.replace(".mat", ".png"), showfig=False)
+    da.plot_2_gaussian(d_mean['CVNN loss'], d_std['CVNN loss'], d_mean['RVNN loss'], d_std['RVNN loss'], 'CVNN', 'RVNN',
+                       x_label='loss', loc='upper right',
+                       title='RVNN vs CVNN loss for ' + os.path.splitext(data_name)[0] + ' dataset',
+                       filename="./results/loss_" + data_name.replace(".mat", ".png"), showfig=False)
+    da.plot_csv_histogram_matplotlib(path="./results/", filename=data_name.replace(".mat", ".csv"), visualize=True)
