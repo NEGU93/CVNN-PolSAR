e7fdbfbccc3eaf6941e8647eab375831f17240a3
NEGU93 <joseagustin.barra@gmail.com>
2021-01-26 12:21:57 +0100

    MSTAR reading of dataset done (I think)

--- src/MSTAR/mstar_preprocessing.py
+++ src/MSTAR/mstar_preprocessing.py
@@ -3,12 +3,13 @@ import re
 import numpy as np
 from pdb import set_trace
 import os
+from tqdm import tqdm
+from pprint import pprint
 
 root_path = Path('/media/barrachina/data/datasets/MSTAR/')
 stm = {     # State machine for reading file. In order!
     'init': {'next': 'reading_header'},
-    'reading_header': {'next': 'reading_data'},
-    'reading_data': {'next': 'end'},
+    'reading_header': {'next': 'end'},
     'end': {'next': None}
 }
 
@@ -31,17 +32,30 @@ def read_file(file):
                 if b'PhoenixHeaderVer' in line:
                     state = stm[state]['next']
             elif state == 'reading_header':
-                if b'EndofPhoenixHeader' in line:
-                    state = stm[state]['next']
-                    data = np.fromfile(f, dtype='>f4')
+                if b'EndofPhoenixHeader' in line:   # End of the header reached
+                    state = stm[state]['next']      # Next state
+                    # All that it's left is to read the data so why not read it now?
+                    tmp_data = np.fromfile(f, dtype='>f4')
+                    assert len(tmp_data) % 2 == 0, "Error, obtained array had not a pair number of elements."
+                    magnitude = tmp_data[:int(len(tmp_data)/2)]
+                    direction = np.exp(1j*tmp_data[int(len(tmp_data)/2):])
+                    data = np.multiply(magnitude, direction)    # Element-wise multiplication
+                    data = np.reshape(data, (metadata['NumberOfRows'], metadata['NumberOfColumns']))
                 else:
-                    key, value = line.decode('utf-8').split('= ')   # Convert it to string and separate key and value
-                    metadata[key] = value.rstrip("\n")              # Remove newline from value
-            elif state == 'reading_data':
-                print("Never get here")
-                # data = np.fromfile(f, dtype='>f4')
-    if state != 'reading_data':
-        print(f"Error: Incomplete data on file: {file}")
+                    key, str_value = line.decode('utf-8').split('= ')  # Convert it to string and separate key and value
+                    str_value = str_value.rstrip("\n")
+                    try:
+                        value = int(str_value)
+                    except ValueError:
+                        try:
+                            value = float(str_value)
+                        except ValueError:
+                            value = str_value
+                    metadata[key] = value              # Remove newline from value
+            elif state == 'end':
+                raise IOError("np.fromfile didn't read everything")
+    if state != 'end':
+        raise IOError(f"Error: Incomplete data on file: {file}")
     return data, metadata
 
 
@@ -50,11 +64,24 @@ def get_dataset():
     total_files = len(data_files)
     metadata = [{} for _ in range(total_files)]
     data = []
-    for file_idx, file in enumerate(data_files):
+    for file_idx, file in enumerate(tqdm(data_files)):
+        path = Path(file).parent.absolute()
         dat, meta = read_file(file)
         metadata[file_idx] = meta
+        metadata[file_idx]['path'] = str(path).replace(str(root_path), '')
         data.append(dat)
+    assert len(metadata) == len(data), "An error occurred, metadata has different size than data"
+    return metadata, data
+
+
+def save_dataset(metadata, data, path):
+    np_data = np.array(data, dtype=object)
+    np_metadata = np.array(metadata)
+    np.save(path/'data.npy', np_data)
+    np.save(path/'metadata.npy', np_metadata)
 
 
 if __name__ == '__main__':
-    get_dataset()
+    metadata, data = get_dataset()
+    save_dataset(metadata, data, root_path)
+
