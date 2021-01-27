f2e7eb7a83caf4ecb589c856cda9595bba184863
NEGU93 <joseagustin.barra@gmail.com>
2021-01-27 12:31:47 +0100

    prettier display to compare jpg

--- src/MSTAR/mstar_utils.py
+++ src/MSTAR/mstar_utils.py
@@ -4,21 +4,29 @@ import matplotlib.pyplot as plt
 from pdb import set_trace
 import os
 from sklearn import preprocessing
-from PIL import Image
+import matplotlib.image as mpimg
+
+root_path = Path('/media/barrachina/data/datasets/MSTAR/')
 
 
 def plot_data(data):
     data = np.abs(data)
     arr = np.asarray(data)
-    plt.imshow(arr, cmap='gray', vmin=np.min(arr), vmax=np.max(arr))
+    plt.imshow(arr, cmap='gray')
+    plt.show()
+
+
+def plot_jpg(mstar_data):
+    fullfilename = str(root_path) + mstar_data[0]['path'] + '/' + mstar_data[0]['Filename'].upper()
+    jpg_path = os.path.splitext(str(fullfilename))[0] + '.JPG'
+    img = mpimg.imread(jpg_path)
+    imgplot = plt.imshow(img, cmap='gray')
     plt.show()
 
 
 if __name__ == '__main__':
-    root_path = Path('/media/barrachina/data/datasets/MSTAR/')
     mstar_data = np.load(root_path/'data.npy', allow_pickle=True)
     plot_data(mstar_data[0]['data'])
-    fullfilename = str(root_path) + mstar_data[0]['path'] + '/' + mstar_data[0]['Filename'].upper()
-    image = Image.open(os.path.splitext(str(fullfilename))[0] + '.JPG')
-    image.show()
+    plot_jpg(mstar_data)
+
 
