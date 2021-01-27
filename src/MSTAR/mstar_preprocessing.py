4ea423966718a82bd1599b71215a1c20b4e43d38
NEGU93 <joseagustin.barra@gmail.com>
2021-01-27 11:42:03 +0100

    Now added dataframe as an option

--- src/MSTAR/mstar_preprocessing.py
+++ src/MSTAR/mstar_preprocessing.py
@@ -1,10 +1,15 @@
 from pathlib import Path
 import re
 import numpy as np
+import pandas as pd
 from pdb import set_trace
 import os
 from tqdm import tqdm
 from pprint import pprint
+
+"""
+This script is used to read the MSTAR dataset and save it to be easily accesible later
+"""
 
 root_path = Path('/media/barrachina/data/datasets/MSTAR/')
 stm = {     # State machine for reading file. In order!
@@ -15,6 +20,11 @@ stm = {     # State machine for reading file. In order!
 
 
 def get_data_files_list():
+    """
+    Reads all files inside the root file (and sub-folders) and returns the list of those that has
+    3 digit extensions (ex: .015)
+    :return: The list of all files with the 3 digit extensions
+    """
     data_files = []
     for path, subdirs, files in os.walk(root_path):
         for name in files:
@@ -24,6 +34,11 @@ def get_data_files_list():
 
 
 def read_file(file):
+    """
+    Reads the file
+    :param file: File to read
+    :return: Dictionary with all the metadata of the header and the data as the key 'data' of the dictionary.
+    """
     state = 'init'  # Reset state machine
     metadata = {}
     with open(file, "rb") as f:  # Open as binary
@@ -39,8 +54,9 @@ def read_file(file):
                     assert len(tmp_data) % 2 == 0, "Error, obtained array had not a pair number of elements."
                     magnitude = tmp_data[:int(len(tmp_data)/2)]
                     direction = np.exp(1j*tmp_data[int(len(tmp_data)/2):])
-                    data = np.multiply(magnitude, direction)    # Element-wise multiplication
-                    data = np.reshape(data, (metadata['NumberOfRows'], metadata['NumberOfColumns']))
+                    dat = np.multiply(magnitude, direction)    # Element-wise multiplication
+                    dat = np.reshape(dat, (metadata['NumberOfRows'], metadata['NumberOfColumns']))
+                    metadata['data'] = dat
                 else:
                     key, str_value = line.decode('utf-8').split('= ')  # Convert it to string and separate key and value
                     str_value = str_value.rstrip("\n")
@@ -56,32 +72,44 @@ def read_file(file):
                 raise IOError("np.fromfile didn't read everything")
     if state != 'end':
         raise IOError(f"Error: Incomplete data on file: {file}")
-    return data, metadata
+    return metadata
 
 
 def get_dataset():
+    """
+    Get all the MSTAR dataset as a list of dictionaries.
+    :return: A list of dictionaries with all the data. Each dictionary of the list corresponds to a file.
+    """
     data_files = get_data_files_list()
     total_files = len(data_files)
-    metadata = [{} for _ in range(total_files)]
-    data = []
+    data = [{} for _ in range(total_files)]
     for file_idx, file in enumerate(tqdm(data_files)):
         path = Path(file).parent.absolute()
-        dat, meta = read_file(file)
-        metadata[file_idx] = meta
-        metadata[file_idx]['path'] = str(path).replace(str(root_path), '')
-        data.append(dat)
-    assert len(metadata) == len(data), "An error occurred, metadata has different size than data"
-    return metadata, data
+        dat = read_file(file)
+        data[file_idx] = dat
+        data[file_idx]['path'] = str(path).replace(str(root_path), '')
+    assert len(data) == len(data), "An error occurred, metadata has different size than data"
+    return data
 
 
-def save_dataset(metadata, data, path):
-    np_data = np.array(data, dtype=object)
-    np_metadata = np.array(metadata)
-    np.save(path/'data.npy', np_data)
-    np.save(path/'metadata.npy', np_metadata)
+def save_dataset(data, path):
+    """
+    Saves the dataset, if it is a pandas dataframe it will pickle it,
+        if not, it will try to cast to numpy array and save it as .npy.
+    :param data: Data to be saved
+    :param path: Path to save the data
+    :return: None
+    """
+    if isinstance(data, pd.DataFrame):
+        df.to_pickle(path/'data.pkl')
+    else:
+        np.save(path/'data.npy', np.array(data))
 
 
 if __name__ == '__main__':
-    metadata, data = get_dataset()
-    save_dataset(metadata, data, root_path)
+    data = get_dataset()
+    df = pd.DataFrame(data)
+    save_dataset(df, root_path)
+    save_dataset(data, root_path)
+    set_trace()
 
