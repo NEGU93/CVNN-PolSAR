5fc137dfe3ca242b9d3a23aa022c30c321cfca48
NEGU93 <joseagustin.barra@gmail.com>
2021-02-03 12:40:35 +0100

    tunning models

--- src/MSTAR/mstar_train.py
+++ src/MSTAR/mstar_train.py
@@ -9,12 +9,15 @@ from cvnn.utils import create_folder
 from mstar_data_processing import resize_image
 from mstar_utils import plot_history, create_excel_file
 
+# TODO: Select the loss functions
+
 
 config = {
     'model': 'chen',
     'dtype': np.float32,
     'tensorflow': True,
-    'epochs': 10
+    'epochs': 10,
+    'batch_size': 100   # TODO: change it using tf.Dataset?
 }
 
 if config['tensorflow']:
@@ -30,26 +33,34 @@ else:
 
 
 def get_model(verbose: bool = False):
-    model = tf.keras.models.Sequential()
-    if config['tensorflow']:
-        model.add(Input(shape=(128, 128, 1)))  # Always use ComplexInput at the start
+    if config['model'] == 'chen':
+        input_shape = (88, 88)
+        model = get_chen_model(input_shape=input_shape)
+    elif config['model'] == 'ding':
+        input_shape = (128, 128)
+        model = get_ding_model(input_shape=input_shape)
     else:
-        model.add(Input(input_shape=(128, 128, 1)))  # Always use ComplexInput at the start
-    model.add(Conv2D(6, (3, 3), activation='cart_relu'))
-    model.add(AveragePooling2D((2, 2)))
-    model.add(Conv2D(12, (3, 3), activation='cart_relu'))
-    model.add(Flatten())
-    model.add(Dense(108, activation='cart_relu'))
-    model.add(Dense(10, activation='softmax_real'))
-    model.compile(optimizer='adam',
-                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-                  metrics=['accuracy'])
+        input_shape = (128, 128)
+        model = tf.keras.models.Sequential()
+        if config['tensorflow']:
+            model.add(Input(shape=(128, 128, 1)))  # Always use ComplexInput at the start
+        else:
+            model.add(Input(input_shape=(128, 128, 1)))  # Always use ComplexInput at the start
+        model.add(Conv2D(6, (3, 3), activation='cart_relu'))
+        model.add(AveragePooling2D((2, 2)))
+        model.add(Conv2D(12, (3, 3), activation='cart_relu'))
+        model.add(Flatten())
+        model.add(Dense(108, activation='cart_relu'))
+        model.add(Dense(10, activation='softmax_real'))
+        model.compile(optimizer='adam',
+                      loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                      metrics=['accuracy'])
     if verbose:
         model.summary()
-    return model
+    return model, input_shape
 
 
-def get_ding_model(verbose: bool = False, input_shape=(128, 128)):
+def get_ding_model(input_shape=(128, 128)):
     model = tf.keras.models.Sequential()
     if config['tensorflow']:
         model.add(Input(shape=input_shape + (1,)))  # Always use ComplexInput at the start
@@ -67,15 +78,17 @@ def get_ding_model(verbose: bool = False, input_shape=(128, 128)):
     model.add(Flatten(dtype=config['dtype']))
     model.add(Dense(10, activation='softmax_real', dtype=config['dtype']))
 
-    model.compile(optimizer='adam',
+    model.compile(optimizer='SGD',
                   loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
                   metrics=['accuracy'])
-    if verbose:
-        model.summary()
     return model
 
 
-def get_chen_model(verbose: bool = False, input_shape=(128, 128)):
+def get_chen_model(input_shape=(128, 128)):
+    # TODO: I still have to implement the weight decay. Options:
+    #   USing addon: https://www.tensorflow.org/addons/api_docs/python/tfa/optimizers/SGDW
+    #   Add L2-reg
+    # TODO: Change learning rate after 50 epochs
     model = tf.keras.models.Sequential()
     if config['tensorflow']:
         model.add(Input(shape=input_shape + (1,)))  # Always use ComplexInput at the start
@@ -95,33 +108,14 @@ def get_chen_model(verbose: bool = False, input_shape=(128, 128)):
     model.add(Conv2D(128, (5, 5), activation='cart_relu', dtype=config['dtype']))
 
     model.add(Conv2D(10, (3, 3), activation='softmax_real', dtype=config['dtype']))
-    # model.add(complex_layers.ComplexFlatten())
-    # model.add(complex_layers.ComplexDense(10, activation='softmax_real'))
 
-    model.compile(optimizer='adam',
+    model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=0.001, momentum=0.9, name='SGD'),
                   loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
                   metrics=['accuracy'])
-    if verbose:
-        model.summary()
     return model
 
 
-if __name__ == '__main__':
-    if config['dtype'] == np.complex64 and config['tensorflow']:
-        raise ValueError("Tensorflow mode does not support complex dtype")
-
-    # Get model
-    if config['model'] == 'chen':
-        input_shape = (88, 88)
-        model = get_chen_model(input_shape=input_shape, verbose=True)
-    elif config['model'] == 'ding':
-        input_shape = (128, 128)
-        model = get_ding_model(input_shape=input_shape, verbose=True)
-    else:
-        input_shape = (128, 128)
-        model = get_model(True)
-
-    # Get data
+def get_data(input_shape):
     img_train, img_test, labels_train, labels_test = get_train_and_test()
     img_train = np.array([resize_image(img, input_shape=input_shape) for img in img_train])
     img_test = np.array([resize_image(img, input_shape=input_shape) for img in img_test])
@@ -130,12 +124,22 @@ if __name__ == '__main__':
     if config['dtype'] == np.float32:
         img_train == np.abs(img_train)
         img_test == np.abs(img_test)
+    return img_train, img_test, labels_train, labels_test
 
+
+if __name__ == '__main__':
+    if config['dtype'] == np.complex64 and config['tensorflow']:
+        raise ValueError("Tensorflow mode does not support complex dtype")
+    # Get model
+    model, input_shape = get_model(True)
+    # Get data
+    img_train, img_test, labels_train, labels_test = get_data(input_shape=input_shape)
     # Train
     history = model.fit(x=img_train, y=labels_train,
                         validation_data=(img_test, labels_test),
-                        epochs=config['epochs'])
+                        epochs=config['epochs'], batch_size=config['batch_size'])
     plot_history(history)
+    # Save results
     path = create_folder('./log/')
     pd.DataFrame.from_dict(history.history).to_csv(path / 'history.csv', index=False)
     fieldsname = []
