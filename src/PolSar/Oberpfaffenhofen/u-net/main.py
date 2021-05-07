54bdd0775e0590151f08ec2b702364f6368d89b5
NEGU93 <joseagustin.barra@gmail.com>
2021-05-07 19:03:19 +0200

    Added notify to script

--- src/PolSar/Oberpfaffenhofen/u-net/main.py
+++ src/PolSar/Oberpfaffenhofen/u-net/main.py
@@ -3,6 +3,8 @@ import os
 from time import time, strftime, localtime
 from datetime import timedelta
 import tensorflow as tf
+from notify_run import Notify
+import traceback
 from pdb import set_trace
 sys.path.insert(1, '/home/barrachina/Documents/onera/src/PolSar/Oberpfaffenhofen')
 from oberpfaffenhofen_dataset import get_dataset_for_segmentation
@@ -39,7 +41,7 @@ def secondsToStr(elapsed=None):
         return str(timedelta(seconds=elapsed))
 
 
-if __name__ == "__main__":
+def run_model():
     train_dataset, test_dataset = get_dataset_for_segmentation(size=cao_dataset_parameters['sliding_window_size'],
                                                                stride=cao_dataset_parameters['sliding_window_stride'])
     train_dataset = train_dataset.batch(cao_dataset_parameters['batch_size']).map(flip)
@@ -57,6 +59,19 @@ if __name__ == "__main__":
     history = model.fit(x=train_dataset, epochs=2,   # cao_fit_parameters['epochs'],
                         validation_data=test_dataset, shuffle=True, callbacks=callbacks)
     stop = time()
-    print(f'Time: {secondsToStr(stop - start)}')
+    return secondsToStr(stop - start)
+
+
+if __name__ == "__main__":
+    # https://notify.run/c/PGqsOzNQ1cSGdWM7
+    notify = Notify()
+    notify.send('New simulation started')
+    try:
+        time = run_model()
+        notify.send(f"Simulations done in {time}")
+    except Exception as e:
+        notify.send("Error occurred")
+        print(e)
+        traceback.print_exc()
 
 
