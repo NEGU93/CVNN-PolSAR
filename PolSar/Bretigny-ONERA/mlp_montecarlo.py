1ceaf10a4b79266c5af7ddc5c8f679134b043f15
NEGU93 <joseagustin.barra@gmail.com>
2021-06-01 11:21:55 +0200

    Added params for paper

--- PolSar/Bretigny-ONERA/mlp_montecarlo.py
+++ PolSar/Bretigny-ONERA/mlp_montecarlo.py
@@ -43,11 +43,12 @@ def test_output_act():
 
 def train(x_train, y_train, x_val, y_val):
     dataset = Dataset(x=x_train, y=y_train)
-    mlp_run_real_comparison_montecarlo(dataset=dataset, iterations=30,
-                                       epochs=150, batch_size=100, display_freq=1,
+    mlp_run_real_comparison_montecarlo(dataset=dataset, iterations=100,
+                                       epochs=300, batch_size=100, display_freq=1,
                                        optimizer='sgd', dropout=0.5, shape_raw=[100, 50], activation='cart_relu',
                                        polar=None,  # output_activation='softmax_real_with_avg',
                                        debug=False, do_all=True, shuffle=False, tensorboard=False, plot_data=False,
+                                       do_conf_mat=True,
                                        validation_data=(x_val, y_val),
                                        capacity_equivalent=True, equiv_technique='ratio'
                                        )
@@ -61,10 +62,6 @@ if __name__ == "__main__":
         notify.send('Simulating k data')
         x_train, y_train, x_val, y_val = get_k_data()
         train(x_train, y_train, x_val, y_val)
-        notify.send('Simulating coh data')
-        x_train, y_train, x_val, y_val = get_coh_data()
-        train(x_train, y_train, x_val, y_val)
-        notify.send(f"Simulations done")
     except Exception as e:
         notify.send("Error occurred")
         print(e)
