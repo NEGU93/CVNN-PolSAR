65b46e3e074b358d524b86b48121899355a5ee29
NEGU93 <joseagustin.barra@gmail.com>
2021-09-20 13:56:08 +0200

    Reduced epochs

--- PolSar/Oberpfaffenhofen/main.py
+++ PolSar/Oberpfaffenhofen/main.py
@@ -35,7 +35,7 @@ from cvnn.montecarlo import MonteCarlo
 from tensorflow.keras.utils import plot_model
 
 cao_fit_parameters = {
-    'epochs': 800,              # Section 3.3.2
+    'epochs': 500,              # Section 3.3.2
     "channels": 6               # This is either 6 (PolSAR) or 21 (PolInSAR)
 }
 
