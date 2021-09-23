855182a2961106ad7233926c032927c47ce7287b
NEGU93 <joseagustin.barra@gmail.com>
2021-09-23 10:40:34 +0200

    changed run script

--- PolSar/Bretigny-ONERA/run_model.sh
+++ PolSar/Bretigny-ONERA/run_model.sh
@@ -1,4 +1,2 @@
-python3 main.py --complex --dropout None None 0.3
 python3 main.py --complex --coherency --dropout None None 0.3
-python3 main.py --complex --dropout None None None
 python3 main.py --complex --coherency --dropout None None None
