1a0855d0afe08bdc12526257a182c7a639c12822
NEGU93 <joseagustin.barra@gmail.com>
2021-09-28 16:28:11 +0200

    changed simulation defaults

--- PolSar/Bretigny-ONERA/dropout_tests.sh
+++ PolSar/Bretigny-ONERA/dropout_tests.sh
@@ -1,12 +1,11 @@
 
-python main.py --complex --coherency --epochs 50 --early_stop --boxcar 3
 python main.py --complex --coherency --epochs 50 --early_stop --boxcar 1
 python main.py --complex --coherency --epochs 50 --early_stop --boxcar 5
 
-python main.py --complex --coherency --epochs 50 --early_stop --boxcar 3 --dropout 0.2 None 0.2
 python main.py --complex --coherency --epochs 50 --early_stop --boxcar 1 --dropout 0.2 None 0.2
+python main.py --complex --coherency --epochs 50 --early_stop --boxcar 3 --dropout 0.2 None 0.2
 
-python main.py --complex --coherency --epochs 50 --early_stop --boxcar 3 --dropout None None 0.2
 python main.py --complex --coherency --epochs 50 --early_stop --boxcar 1 --dropout None None 0.2
+python main.py --complex --coherency --epochs 50 --early_stop --boxcar 3 --dropout None None 0.2
 
 
--- PolSar/Bretigny-ONERA/run_model.sh
+++ PolSar/Bretigny-ONERA/run_model.sh
@@ -1,4 +1,3 @@
-python3 main.py --complex --dropout 0.2 None 0.2
-python3 main.py --complex --coherency --dropout 0.2 None 0.2
-python3 main.py --complex --coherency --split_datasets --dropout 0.2 None 0.2
-python3 main.py --complex --split_datasets --dropout 0.2 None 0.2
+python3 main.py --tensorflow
+python3 main.py --split_datasets --tensorflow
+
