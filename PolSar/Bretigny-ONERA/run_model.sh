942a19109933964bbfdff3f6f977839e9639535f
NEGU93 <joseagustin.barra@gmail.com>
2021-09-29 17:57:15 +0200

    added metrics to simulation:

--- PolSar/Bretigny-ONERA/run_model.sh
+++ PolSar/Bretigny-ONERA/run_model.sh
@@ -1,4 +1,5 @@
-python3 main.py --complex --epochs 200
 python3 main.py --split_datasets --complex --epochs 200
-python3 main.py --epochs 200
 python3 main.py --split_datasets --epochs 200
+python3 main.py --complex --epochs 200
+python3 main.py --epochs 200
+
