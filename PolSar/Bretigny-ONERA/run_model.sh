f60e5ddf0c6d1028bf187d43fb43fe8108b3a65f
NEGU93 <joseagustin.barra@gmail.com>
2021-10-01 15:45:35 +0200

    changed run_model

--- PolSar/Bretigny-ONERA/run_model.sh
+++ PolSar/Bretigny-ONERA/run_model.sh
@@ -1,3 +1,6 @@
-python3 main.py --split_datasets --complex --epochs 150
-python3 main.py --split_datasets --complex --coherency --epochs 150
-
+while :
+do
+	python3 main.py --split_datasets --complex --epochs 150
+	python3 main.py --split_datasets --complex --coherency --epochs 150
+	sleep 1
+done
