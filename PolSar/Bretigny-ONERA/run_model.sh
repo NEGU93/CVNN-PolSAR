b8a792063e1185fd9d22b14203a912ac79c870b1
NEGU93 <joseagustin.barra@gmail.com>
2021-10-01 18:22:39 +0200

    all simulations done

--- PolSar/Bretigny-ONERA/run_model.sh
+++ PolSar/Bretigny-ONERA/run_model.sh
@@ -1,8 +1,8 @@
 while :
 do
-	python3 main.py --split_datasets --epochs 150
-	python3 main.py --split_datasets --coherency --epochs 150
-	python3 main.py --real_mode --split_datasets --epochs 150
-	python3 main.py --real_mode --split_datasets --coherency --epochs 150
+	python3 main.py --split_datasets --epochs 200
+	python3 main.py --split_datasets --coherency --epochs 200
+	python3 main.py --real_mode --split_datasets --epochs 200
+	python3 main.py --real_mode --split_datasets --coherency --epochs 200
 	sleep 1
 done
