c317003ca7b66d93cabf65bf6a5dcc435231e775
NEGU93 <joseagustin.barra@gmail.com>
2021-10-13 16:19:32 +0200

    great graph!

--- PolSar/Bretigny-ONERA/run_model.sh
+++ PolSar/Bretigny-ONERA/run_model.sh
@@ -1,7 +1,4 @@
-python3 main.py --epochs 50 --early_stop --split_datasets 
-python3 main.py --epochs 50 --early_stop --coherency --split_datasets 
-python3 main.py --epochs 50 --early_stop --coherency --split_datasets --boxcar 1
+python3 main.py --epochs 100 --split_datasets --weighted_loss --coherency
+python3 main.py --epochs 100 --coherency
+python3 main.py --epochs 100 --weighted_loss --coherency
 
-python3 main.py --epochs 50 --early_stop --split_datasets --real_mode --tensorflow
-python3 main.py --epochs 50 --early_stop --coherency --split_datasets --real_mode --tensorflow
-python3 main.py --epochs 50 --early_stop --coherency --split_datasets --boxcar 1 --real_mode --tensorflow
--- PolSar/Bretigny-ONERA/several_plotter.py
+++ PolSar/Bretigny-ONERA/several_plotter.py
@@ -8,16 +8,24 @@ import pandas as pd
 import os
 from pathlib import Path
 from pdb import set_trace
-from typing import Optional
+from typing import Optional, Dict
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QRadioButton, QLabel, QVBoxLayout, QHBoxLayout, \
+    QButtonGroup, QSlider, QTableView
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
+from PyQt5 import QtCore
+import sys
+
 
 params = {
     "dtype": 'complex',
-    "model": 'default',
+    "model": '0',
     "library": 'cvnn',
     "dataset_mode": 'k',
     "dataset_split": 'random',
-    "boxcar": 'default',
+    "boxcar": '3',
     "loss": 'conventional',
+    "labels": 'original'
 }
 keys = ['accuracy', 'val_accuracy']
 
@@ -50,12 +58,13 @@ def get_paths(root_dir: str = "/media/barrachina/data/results/Bretigny/after_ica
                             "image": str(Path(child_dir[0]) / 'prediction.png'),
                             "params": {
                                 "dtype": f"{'real' if 'real_mode' in simu_params else 'complex'}",
-                                "model": f"{simu_params.split()[model_index + 1] if model_index != -1 else 'default'}",
+                                "model": f"{simu_params.split()[model_index + 1] if model_index != -1 else '0'}",
                                 "library": f"{'cvnn' if 'tensorflow' not in simu_params else 'tensorflow'}",
                                 "dataset_mode": f"{'coh' if 'coherency' in simu_params else 'k'}",
                                 "dataset_split": f"{'sections' if 'split_datasets' in simu_params else 'random'}",
-                                "boxcar": f"{simu_params.split()[boxcar_index + 1] if boxcar_index != -1 else 'default'}",
+                                "boxcar": f"{simu_params.split()[boxcar_index + 1] if boxcar_index != -1 else '3'}",
                                 "loss": f"{'conventional' if 'weighted_loss' not in simu_params else 'weighted'}",
+                                "labels": f"{'balanced' if 'balanced' in child_dir[0] else 'original'}"
                             }
                         }
                     else:
@@ -416,10 +425,316 @@ def live_plot():
     fig.show()
 
 
-if __name__ == '__main__':
-    live_plot()
+"""
+    Qt methods
+"""
 
-    # img = imread(user_dict['/media/barrachina/data/results/Bretigny/after_icassp/08Friday/run-17h03m24']['image'])
-    # fig = px.imshow(img, layout=layout)
-    # fig.show()
+
+class DataFrameModel(QtCore.QAbstractTableModel):
+    DtypeRole = QtCore.Qt.UserRole + 1000
+    ValueRole = QtCore.Qt.UserRole + 1001
+
+    def __init__(self, df=pd.DataFrame(), parent=None):
+        super(DataFrameModel, self).__init__(parent)
+        self._dataframe = df
+
+    def setDataFrame(self, dataframe):
+        self.beginResetModel()
+        self._dataframe = dataframe.copy()
+        self.endResetModel()
+
+    def dataFrame(self):
+        return self._dataframe
+
+    dataFrame = QtCore.pyqtProperty(pd.DataFrame, fget=dataFrame, fset=setDataFrame)
+
+    @QtCore.pyqtSlot(int, QtCore.Qt.Orientation, result=str)
+    def headerData(self, section: int, orientation: QtCore.Qt.Orientation, role: int = QtCore.Qt.DisplayRole):
+        if role == QtCore.Qt.DisplayRole:
+            if orientation == QtCore.Qt.Horizontal:
+                return self._dataframe.columns[section]
+            else:
+                return str(self._dataframe.index[section])
+        return QtCore.QVariant()
+
+    def rowCount(self, parent=QtCore.QModelIndex()):
+        if parent.isValid():
+            return 0
+        return len(self._dataframe.index)
+
+    def columnCount(self, parent=QtCore.QModelIndex()):
+        if parent.isValid():
+            return 0
+        return self._dataframe.columns.size
+
+    def data(self, index, role=QtCore.Qt.DisplayRole):
+        if not index.isValid() or not (0 <= index.row() < self.rowCount() \
+            and 0 <= index.column() < self.columnCount()):
+            return QtCore.QVariant()
+        row = self._dataframe.index[index.row()]
+        col = self._dataframe.columns[index.column()]
+        dt = self._dataframe[col].dtype
+
+        val = self._dataframe.iloc[row][col]
+        if role == QtCore.Qt.DisplayRole:
+            return str(val)
+        elif role == DataFrameModel.ValueRole:
+            return val
+        if role == DataFrameModel.DtypeRole:
+            return dt
+        return QtCore.QVariant()
+
+    def roleNames(self):
+        roles = {
+            QtCore.Qt.DisplayRole: b'display',
+            DataFrameModel.DtypeRole: b'dtype',
+            DataFrameModel.ValueRole: b'value'
+        }
+        return roles
+
+    def flags(self, index) -> Qt.ItemFlags:
+        # QtCore.Qt.ItemIsEditable |
+        # | QtCore.Qt.ItemIsUserCheckable | QtCore.Qt.ItemIsDragEnabled | QtCore.Qt.ItemIsDropEnabled
+        # | QtCore.Qt.ItemIsTristate
+        return QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Results")
+        self.label_image = QLabel()
+        self.image_paths = get_paths()
+        self.params = params
+        self.params_label = QLabel(str(self.params))
+        df = pd.DataFrame()
+        for img in self.image_paths.values():
+            df = pd.concat([df, pd.DataFrame(img['params'], index=[0])], ignore_index=True)
+        df.sort_values(by=['labels', 'dtype', 'dataset_mode', 'dataset_split', 'loss', 'boxcar'])
+        self.btngroup = []
+        widget = QWidget()
+        hlayout = QHBoxLayout()
+
+        self.get_image('')
+        hlayout.addLayout(self.radiobuttons())
+        img_layout = QVBoxLayout()
+        self.params_label.setAlignment(Qt.AlignCenter)
+        img_layout.addWidget(self.params_label)
+        img_layout.addWidget(self.label_image)
+        hlayout.addLayout(img_layout)
+
+        outer_layout = QVBoxLayout()
+        outer_layout.addLayout(hlayout)
+        self.tableView = QTableView()
+        # self.verticalLayout.addWidget(self.tableView)
+        self.tableView.setModel(DataFrameModel(df))
+        outer_layout.addWidget(self.tableView)
+        widget.setLayout(outer_layout)
+        self.setCentralWidget(widget)
+        self.show()
+
+    def radiobuttons(self):
+        vlayout = QVBoxLayout()
+
+        vlayout.addLayout(self.add_title(self.dtype_radiobuttons(), "Dtype"))
+        vlayout.addLayout(self.add_title(self.library_radiobuttons(), "Library"))
+        vlayout.addLayout(self.add_title(self.dataset_mode_radiobuttons(), "Dataset Mode"))
+        vlayout.addLayout(self.add_title(self.dataset_split_radiobuttons(), "Dataset Split"))
+        vlayout.addLayout(self.add_title(self.loss_radiobuttons(), "Loss"))
+        vlayout.addLayout(self.add_title(self.labels_radiobuttons(), "Loss"))
+        vlayout.addLayout(self.model_slider())
+        vlayout.addLayout(self.boxcar_slider())
+
+        return vlayout
+
+    def add_title(self, layout, name: str, up: bool = True):
+        lay = QVBoxLayout()
+        l1 = QLabel(name)
+        l1.setAlignment(Qt.AlignCenter)
+        if up:
+            lay.addWidget(l1)
+        lay.addLayout(layout)
+        if not up:
+            lay.addWidget(l1)
+        return lay
+
+    def dtype_radiobuttons(self):
+        vlayout = QHBoxLayout()
+        rb1 = QRadioButton("complex", self)
+        rb1.toggled.connect(lambda: self.update_image("dtype", rb1.text()))
+
+        rb2 = QRadioButton("real", self)
+        rb2.toggled.connect(lambda: self.update_image("dtype", rb2.text()))
+
+        self.rb = rb2
+
+        self.btngroup.append(QButtonGroup())
+        self.btngroup[-1].addButton(rb2)
+        self.btngroup[-1].addButton(rb1)
+
+        rb1.setChecked(True)
+        vlayout.addWidget(rb1)
+        vlayout.addWidget(rb2)
+
+        return vlayout
+
+    def library_radiobuttons(self):
+        vlayout = QHBoxLayout()
+        rb1 = QRadioButton("cvnn", self)
+        rb1.toggled.connect(lambda: self.update_image("library", rb1.text()))
+
+        rb2 = QRadioButton("tensorflow", self)
+        rb2.toggled.connect(lambda: self.update_image("library", rb2.text()))
+
+        self.btngroup.append(QButtonGroup())
+        self.btngroup[-1].addButton(rb2)
+        self.btngroup[-1].addButton(rb1)
+
+        rb1.setChecked(True)
+        vlayout.addWidget(rb1)
+        vlayout.addWidget(rb2)
+
+        return vlayout
+
+    def labels_radiobuttons(self):
+        vlayout = QHBoxLayout()
+        rb1 = QRadioButton("original", self)
+        rb1.toggled.connect(lambda: self.update_image("labels", rb1.text()))
+
+        rb2 = QRadioButton("balanced", self)
+        rb2.toggled.connect(lambda: self.update_image("labels", rb2.text()))
+
+        self.btngroup.append(QButtonGroup())
+        self.btngroup[-1].addButton(rb2)
+        self.btngroup[-1].addButton(rb1)
+
+        rb1.setChecked(True)
+        vlayout.addWidget(rb1)
+        vlayout.addWidget(rb2)
+
+        return vlayout
+
+    def dataset_mode_radiobuttons(self):
+        vlayout = QHBoxLayout()
+        rb1 = QRadioButton("k", self)
+        rb1.toggled.connect(lambda: self.update_image("dataset_mode", rb1.text()))
+
+        rb2 = QRadioButton("coh", self)
+        rb2.toggled.connect(lambda: self.update_image("dataset_mode", rb2.text()))
+
+        self.btngroup.append(QButtonGroup())
+        self.btngroup[-1].addButton(rb2)
+        self.btngroup[-1].addButton(rb1)
+
+        rb1.setChecked(True)
+        vlayout.addWidget(rb1)
+        vlayout.addWidget(rb2)
+
+        return vlayout
+
+    def dataset_split_radiobuttons(self):
+        vlayout = QHBoxLayout()
+        rb1 = QRadioButton("random", self)
+        rb1.toggled.connect(lambda: self.update_image("dataset_split", rb1.text()))
+
+        rb2 = QRadioButton("sections", self)
+        rb2.toggled.connect(lambda: self.update_image("dataset_split", rb2.text()))
+
+        self.btngroup.append(QButtonGroup())
+        self.btngroup[-1].addButton(rb2)
+        self.btngroup[-1].addButton(rb1)
+
+        rb1.setChecked(True)
+        vlayout.addWidget(rb1)
+        vlayout.addWidget(rb2)
+
+        return vlayout
+
+    def loss_radiobuttons(self):
+        vlayout = QHBoxLayout()
+        rb1 = QRadioButton("conventional", self)
+        rb1.toggled.connect(lambda: self.update_image("loss", rb1.text()))
+
+        rb2 = QRadioButton("weighted", self)
+        rb2.toggled.connect(lambda: self.update_image("loss", rb2.text()))
+
+        self.btngroup.append(QButtonGroup())
+        self.btngroup[-1].addButton(rb2)
+        self.btngroup[-1].addButton(rb1)
+
+        rb1.setChecked(True)
+        vlayout.addWidget(rb1)
+        vlayout.addWidget(rb2)
+
+        return vlayout
+
+    def model_slider(self):
+        layout = QVBoxLayout()
+        l1 = QLabel("Model")
+        l1.setAlignment(Qt.AlignCenter)
+        layout.addWidget(l1)
+        sl = QSlider(Qt.Horizontal)
+        sl.setMinimum(0)
+        sl.setMaximum(4)
+        sl.setValue(0)
+        sl.setTickInterval(1)
+        sl.setTickPosition(QSlider.TicksBelow)
+        sl.valueChanged[int].connect(lambda: self.update_image("model", str(sl.value())))
+        layout.addWidget(sl)
+        return layout
+
+    def boxcar_slider(self):
+        layout = QVBoxLayout()
+        l1 = QLabel("Boxcar")
+        l1.setAlignment(Qt.AlignCenter)
+        layout.addWidget(l1)
+        sl = QSlider(Qt.Horizontal)
+        sl.setMinimum(1)
+        sl.setMaximum(5)
+        sl.setValue(3)
+        sl.setTickInterval(2)
+        sl.setTickPosition(QSlider.TicksBelow)
+        sl.valueChanged[int].connect(lambda: self.update_image("boxcar", str(sl.value())))
+        layout.addWidget(sl)
+        return layout
+
+    def get_image(self, image_path: str):
+        if image_path == '':
+            pixmap = QPixmap('/media/barrachina/data/datasets/PolSar/Bretigny-ONERA/bret-2003.png')
+        else:
+            pixmap = QPixmap(image_path)
+        scaled_pixmap = pixmap.scaledToWidth(1000)
+        self.label_image.setPixmap(scaled_pixmap)
+        self.label_image.resize(scaled_pixmap.width(), scaled_pixmap.height())
+        # self.show()
+
+    def update_image(self, key, value):
+        self.params[key] = value
+        self.params_label.setText(str(self.params))
+        # if value == "tensorflow":
+        #     self.rb.setChecked(True)
+        #     self.params["dtype"] = "real"       # Not sure if this is necessary but it wont hurt
+        path = ""
+        for img_path in self.image_paths.values():
+            if self._compare_params(img_path['params']):
+                path = img_path['image']
+                break
+        self.get_image(path)
+
+    def _compare_params(self, param: Dict[str, str]) -> bool:
+        for key in param:
+            if param[key] != self.params[key]:
+                if key == "boxcar" and param["dataset_mode"] == 'k':
+                    continue    # saved
+                else:
+                    return False
+        return True
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    app.exec()
 
