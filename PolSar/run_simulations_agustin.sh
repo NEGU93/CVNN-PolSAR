118f00197b89787cef7fec240bbe200586a0e104
2019barrachija <jose-agustin.barrachina@centralesupelec.fr>
2021-11-15 14:35:57 +0100

    Added chengfang batch

--- PolSar/run_simulations_agustin.sh
+++ PolSar/run_simulations_agustin.sh
@@ -1,6 +1,6 @@
 epochs=100
 for i in {1..50};
 do
-	python principal_simulation.py --epochs $epochs --dataset_method random --model cao --balance none --dataset BRET
+		python3 principal_simulation.py --epochs $epochs --dataset_method random --model cao --balance none --dataset BRET
 done
 
