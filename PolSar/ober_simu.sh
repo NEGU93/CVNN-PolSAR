b5c455db88eb03192eeefcd483f0f083ce0cab55
NEGU93 <joseagustin.barra@gmail.com>
2021-12-07 09:55:25 +0100

    removed some sh files

--- PolSar/ober_simu.sh
+++ PolSar/ober_simu.sh
@@ -1,8 +1,20 @@
-max=20
+
 epochs=300
-for iteration in `seq 2 $max`
-do
-	python3 principal_simulation.py --coherency --epochs $epochs --dataset_method random --model cao --early_stop 50 --real_mode real_imag --dataset OBER
-	python3 principal_simulation.py --coherency --epochs $epochs --dataset_method random --model cao --early_stop 50 --real_mode real_imag --dataset OBER --tensorflow
-	python3 principal_simulation.py --coherency --epochs $epochs --dataset_method random --model cao --early_stop 50 --dataset OBER
-done
+
+python3 principal_simulation.py --coherency --epochs $epochs --dataset_method random --model cao --real_mode real_imag --dataset OBER --tensorflow
+python3 principal_simulation.py --coherency --epochs $epochs --dataset_method random --model cao --dataset OBER
+python3 principal_simulation.py --coherency --epochs $epochs --dataset_method random --model cao --real_mode real_imag --dataset OBER
+python3 principal_simulation.py --coherency --epochs $epochs --dataset_method random --model cao --real_mode real_imag --dataset OBER --tensorflow
+python3 principal_simulation.py --coherency --epochs $epochs --dataset_method random --model cao --dataset OBER
+python3 principal_simulation.py --coherency --epochs $epochs --dataset_method random --model cao --real_mode real_imag --dataset OBER
+python3 principal_simulation.py --coherency --epochs $epochs --dataset_method random --model cao --real_mode real_imag --dataset OBER --tensorflow
+python3 principal_simulation.py --coherency --epochs $epochs --dataset_method random --model cao --dataset OBER
+python3 principal_simulation.py --coherency --epochs $epochs --dataset_method random --model cao --real_mode real_imag --dataset OBER
+python3 principal_simulation.py --coherency --epochs $epochs --dataset_method random --model cao --real_mode real_imag --dataset OBER --tensorflow
+python3 principal_simulation.py --coherency --epochs $epochs --dataset_method random --model cao --dataset OBER
+python3 principal_simulation.py --coherency --epochs $epochs --dataset_method random --model cao --real_mode real_imag --dataset OBER
+python3 principal_simulation.py --coherency --epochs $epochs --dataset_method random --model cao --real_mode real_imag --dataset OBER --tensorflow
+python3 principal_simulation.py --coherency --epochs $epochs --dataset_method random --model cao --dataset OBER
+python3 principal_simulation.py --coherency --epochs $epochs --dataset_method random --model cao --real_mode real_imag --dataset OBER
+
+
--- PolSar/run_simulations_agustin.sh
+++
@@ -1,6 +0,0 @@
-epochs=70
-for i in {1..50};
-do
-		python3 principal_simulation.py --epochs $epochs --dataset_method random --model cao --balance none --dataset BRET
-done
-
