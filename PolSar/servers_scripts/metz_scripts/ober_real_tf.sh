0fd8d21282bb34d0362e3a81a4518dd95339a33e
NEGU93 <joseagustin.barra@gmail.com>
2021-12-13 11:26:50 +0100

    added tf ober run script

---
+++ PolSar/servers_scripts/metz_scripts/ober_real_tf.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#SBATCH --job-name=ober_real
+#SBATCH --nodes=1
+#SBATCH --partition=gpu_prod_long
+#SBATCH --time=24:00:00
+#SBATCH --mail-user=joseagustin.barra@gmail.com 
+#SBATCH --mail-type=ALL
+#SBATCH --output=logs/slurm-%j.out
+#SBATCH --error=logs/slurm-%j.err
+
+python3 ../../principal_simulation.py --tensorflow --real_imag --coherency --epochs 400 --dataset_method random --model cao --dataset OBER
+
+
