56a42ccf4a23c02903fae3f166061fc2b7eb534e
NEGU93 <joseagustin.barra@gmail.com>
2021-12-16 10:56:50 +0100

    ok

--- PolSar/servers_scripts/metz_scripts/script.py
+++ PolSar/servers_scripts/metz_scripts/script.py
@@ -45,7 +45,7 @@ if __name__ == "__main__":
     with open(str(json_path)) as json_file:
         config_json = json.load(json_file)
 
-    for iterations in range(5):
+    for iterations in range(1):
         for param in config_json:
             # Launch the batch jobs
             param_str = get_params(param)
